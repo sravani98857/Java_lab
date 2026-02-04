7f79daf134c32c5c2753e297d02e98bf8c7556d2
sravani98857 <katravathsravani22@gmail.com>
2026-02-04 10:56:48 +0530

    Add files via upload

---
+++ TotalAverage.java
@@ -0,0 +1,27 @@
+class TotalAverage 
+{
+	static int a;
+	static int b;
+	static int total;
+	static double average;
+	static void calculate()
+	{
+		total=a+b;
+		average=total/2.0;
+	}
+	
+	public static void main(String args[])
+	{
+		// command line aruguments //
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		
+	
+	calculate();
+	
+		System.out.println("first number:"+a);
+		System.out.println("second number:"+b);
+		System.out.println("total:"+total);
+		System.out.println("average:"+average);
+}
+}
